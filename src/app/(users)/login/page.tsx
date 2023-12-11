e450a7525712890b95b11144746cee693864b3dd
MSK1206 <phantom7216@yahoo.co.jp>
2023-12-11 23:06:22 +0900

    Add: CSSの軽微な修正

--- src/app/(users)/login/page.tsx
+++ src/app/(users)/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from '@/app/components/ui/Login-Form';
 
 export default function LoginPage() {
   return (
-    <main className="flex items-center justify-center md:h-screen">
+    <main className="min-h-screen flex items-center justify-center">
       <LoginForm />
     </main>
   );
